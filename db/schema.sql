a32361e820a0f8060e3e2c80cdfa4269fc263055
Magus <mfail792@gmail.com>
2019-10-14 20:35:43 -0500

    removed timestamp column

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varChar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
  );
 
