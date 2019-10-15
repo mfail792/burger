8c5947867e92590bad9d9c995da8520784de7db3
Magus <mfail792@gmail.com>
2019-10-14 22:10:22 -0500

    updated JAWSDB connection info

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.nvJAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
@@ -14,13 +14,5 @@ if (process.nvJAWSDB_URL) {
 }
 
 
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 var express = require("express");
 
 var PORT = process.env.PORT || 3000;
+
+
 
 var app = express();
 
